2d250c5e19fd64ecafb0e0e5e85561a9d272d204
Chris Bandrowsky <chris.bandrowsky@gmail.com>
2017-05-09 15:53:30 -0600

    people data cleaned. working on turning to array.

--- src/Components/Helpers/helper.js
+++ src/Components/Helpers/helper.js
@@ -3,6 +3,53 @@ export default class Helper{
     this.data = data || ''
   }
 
+  cleanCrawl(data){
+    let cleanData = {
+      crawl: data.opening_crawl,
+      release: data.release_date,
+      title: data.title
+    }
+    return cleanData
+  }
+
+  cleanPeople(data){
+    let cleaned = data.results.reduce((acc, person) =>{
+      if(!acc[person.name]){
+        acc[person.name] = {}
+        acc[person.name].name = person.name
+
+        fetch(person.homeworld)
+          .then((response) => response.json())
+          .then((planet) =>{
+            acc[person.name].homeworld = planet.name
+            acc[person.name].population = planet.population
+            return
+          })
+        fetch(person.species)
+          .then((response) => response.json())
+          .then((type) =>{
+            acc[person.name].species = type.name
+            acc[person.name].language = type.language
+            return
+          })
+        console.log(acc)
+      } return acc
+    }, {})
+
+    // let array = Object.keys(cleaned).map((key) =>{
+    //   return key[cleaned]
+    // })
+    //
+    // console.log(array)
+    return cleaned
+  }
+
+  planetMatch(){
+    //api call
+    //concat homeworld into string for api call
+    //return homeworldpop
+  }
+
   randomNumber(){
     let num = Math.floor(Math.random() * (7- 1)) + 1;
     return num
