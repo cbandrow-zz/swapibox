2073868f9c46110742d91c9eb7f711c7f04dab51
Chris Bandrowsky <chris.bandrowsky@gmail.com>
2017-05-10 14:11:54 -0600

    api changes

--- src/Components/People/PeopleCard.js
+++ src/Components/People/PeopleCard.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import './People.css';
-const PeopleCard = ({data}) =>{
+const PeopleCard = ({data, addFavorite}) =>{
 
 
   return(
     <div className = 'peopleCardInfo'>
       <h2>{data.name}</h2>
+      <div className = "favoriteDiv"
+           onClick = {()=>addFavorite(data.name)}>
+        <p>FAV</p>
+      </div>
       <p>{data.homeworld}</p>
       <p>{data.species}</p>
       <p>{data.language}</p>
--- src/Components/Vehicles/VehicleCard.js
+++ src/Components/Vehicles/VehicleCard.js
@@ -4,8 +4,11 @@ const VehicleCard = ({data}) =>{
 
 
   return(
-    <div>
+    <div className = "vehicleCardInfo">
       <h2>{data.name}</h2>
+      <div className = "favoriteDiv">
+        <p>FAV</p>
+      </div>
       <p>{data.model}</p>
       <p>{data.class}</p>
       <p>{data.passengers}</p>
