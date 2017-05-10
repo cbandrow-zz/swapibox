7d28e644162cef7c24c62c1164574a952ed9c7aa
Breana Knight <Akewlbreeze.bc@gmail.com>
2017-05-10 13:38:10 -0600

    adds stub files, and sets up test files for components

---
+++ src/Components/Card/Card.test.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import Card from './Card.js';
+import { shallow, mount } from 'enzyme';
+import ReactDOM from 'react-dom';
---
+++ src/Components/Vehicles/Vehicles.test.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import VehicleCard from './VehicleCard.js';
+import { shallow, mount } from 'enzyme';
+import ReactDOM from 'react-dom';
+import PeopleData from '../Components/Helpers/VehicleData.js'
