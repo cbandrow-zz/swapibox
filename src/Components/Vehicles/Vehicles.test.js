530d6a2ee832b86231344fc8730b4d472d3b6db5
Breana Knight <Akewlbreeze.bc@gmail.com>
2017-05-12 00:06:29 -0600

    testing for vehicles, planets, people and story scroll and category initiated

--- src/Components/Vehicles/Vehicles.test.js
+++ src/Components/Vehicles/Vehicles.test.js
@@ -1,6 +1,39 @@
-import React from 'react'
-import VehicleCard from './VehicleCard.js';
-import { shallow, mount } from 'enzyme';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import VehicleCard from "./VehicleCard.js";
+import fetchMock from "fetch-mock";
+import { shallow, mount } from "enzyme";
+import mockVehicle from "../Stubs/VehicleData.json";
+import Helper from "../Helpers/helper.js";
 
-import fetchMock from 'fetch-mock'
+describe("Vehicles", () => {
+
+  it("has a class of vehicleCardInfo", () => {
+    const mockClick = jest.fn();
+    const wrapper = shallow(
+      <VehicleCard data={mockVehicle} addFavorite={mockClick} favorites={[]} />
+    );
+
+    expect(wrapper.find(".vehicleCardInfo").length).toBe(1);
+  });
+
+  it("evaluates the number of card elements ", () => {
+    const mockClick = jest.fn();
+    const wrapper = shallow(
+      <VehicleCard data={mockVehicle} addFavorite={mockClick} favorites={[]} />
+    );
+
+    expect(wrapper.find("p").length).toBe(3);
+  });
+
+  it("checks to see that favorites function has been called", () => {
+    const mockClick = jest.fn();
+    const wrapper = shallow(
+      <VehicleCard data={mockVehicle} addFavorite={mockClick} favorites={[]} />
+    );
+
+    const button = wrapper.find(".vehicleCardInfo").children().first();
+    button.simulate("click");
+    expect(mockClick).toBeCalled();
+  });
+});
